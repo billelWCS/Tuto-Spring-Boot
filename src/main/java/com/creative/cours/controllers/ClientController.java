d46f1026d974fcb42f9d4b8177fc5ce57c2aab58
Billel <billel.abbes@hotmail.com>
2023-09-18 16:14:40 +0200

    ReposneEntity

--- src/main/java/com/creative/cours/controllers/ClientController.java
+++ src/main/java/com/creative/cours/controllers/ClientController.java
@@ -3,9 +3,13 @@ package com.creative.cours.controllers;
 import com.creative.cours.models.Client;
 import com.creative.cours.repositories.ClientRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @RestController
@@ -15,20 +19,34 @@ public class ClientController {
     ClientRepo clientRepo;
 
     @GetMapping(value = "/")
-    public List<Client> getAllClients(){
-        return clientRepo.findAll();
+    public ResponseEntity<List<Client>> getAllClients(){
+        return new ResponseEntity<List<Client>>(
+                clientRepo.findAll(),
+                HttpStatus.OK
+        );
     }
 
     @GetMapping(value = "{id}")
-    public Client getClientById(@PathVariable Long id){
+    public ResponseEntity<?> getClientById(@PathVariable Long id){
         Optional<Client> client = clientRepo.findById(id);
+        Map<String, String> body = new HashMap<>();
         if (client.isPresent())
-            return client.get();
-        return null;
+            return new ResponseEntity<>(
+                    client.get(),
+                    HttpStatus.OK
+            );
+            body.put("message", "Client not found");
+            return new ResponseEntity<>(
+                    body,
+                    HttpStatus.NOT_FOUND
+            );
     }
 
     @PostMapping(value = "/")
-    public void createClient(@RequestBody Client client){
-        clientRepo.save(client);
+    public ResponseEntity<Client> createClient(@RequestBody Client client){
+        return new ResponseEntity<Client>(
+                clientRepo.save(client),
+                HttpStatus.CREATED
+        );
     }
 }
